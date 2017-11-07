bebf7388643944f80d05302147dccca87b254fb0
Daniel Beyer <dan.beyer@yandex.com>
2017-11-07 04:15:14 +0100

    add link

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Home } from '../home/home';
+import { Validators, FormBuilder, FormGroup  } from '@angular/forms';
 
+import { Home } from '../home/home';
 import { SignupPage } from '../signup/signup';
 import { FinishsignPage } from '../finishsign/finishsign';
 
@@ -15,7 +16,20 @@ export class Register {
 
   signup = SignupPage;
   finishsign=FinishsignPage;
+  digitcode:any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  slideOneForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,public formBuilder: FormBuilder) {
+  	this.digitcode="";
+
+  	this.slideOneForm = formBuilder.group({
+        digitcode: ['']
+    });
+  }
+
+  codelimit(){
+  	
+  }
 
 }
